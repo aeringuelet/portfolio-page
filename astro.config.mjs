c77167035a8202e8f8bf16c4d992262d7cedb5b0
AE Ringuelet <eze87.lp@hotmail.com>
2025-12-11 19:56:45 +0100

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
-	site: "https://aeringuelet.github.io",
+	site: "https://aeringuelet.github.io/portfolio-page",
 	vite: {
 		plugins: [tailwindcss()],
 	},
